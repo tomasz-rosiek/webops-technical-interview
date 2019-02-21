84a8a6945be7b922b3a9f8542b2887af8e8a367c
bernie camus <bernie.camus@digital.hmrc.gov.uk>
2019-02-21 14:21:24 +0000

    Fix ssh access (99->22)

--- modules/security_groups/security_linux.tf
+++ modules/security_groups/security_linux.tf
@@ -19,8 +19,8 @@ resource "aws_security_group" "linux_servers" {
 
 resource "aws_security_group_rule" "ssh" {
   type              = "ingress"
-  from_port         = 99
-  to_port           = 99
+  from_port         = 22
+  to_port           = 22
   protocol          = "TCP"
   cidr_blocks       = ["${compact(var.allowed_cidr_blocks)}"]
   security_group_id = "${aws_security_group.linux_servers.id}"
